f250b5c74d9d3829239f1e36715591071b64527e
rqh3121 <muhammadkurniasandi26@gmail.com>
2026-03-30 16:00:56 +0700

    label_03

--- shipping_label_db.sql
+++ shipping_label_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 30, 2026 at 07:43 AM
+-- Generation Time: Mar 30, 2026 at 08:59 AM
 -- Server version: 8.0.30
 -- PHP Version: 7.4.33
 
@@ -50,7 +50,10 @@ CREATE TABLE `shipments` (
 --
 
 INSERT INTO `shipments` (`id`, `sender_name`, `sender_contact`, `sender_address`, `receiver_name`, `receiver_contact`, `receiver_address`, `receiver_city`, `package_count`, `item_description`, `resi_number`, `expedition`, `resi_photo`, `created_at`, `updated_at`) VALUES
-(2, 'PT PELITA INDONESIA DJAYA', '+62 851-8307-3715', 'Jl. Raya Angkasa No.18, Kelurahan Gunung Sahari Selatan, Kemayoran, Jakarta Pusat 10610.', 'Rahmat Usman', '+62 852-4466-1641', 'Kantor pelni Nabire\r\nJalan frans kasisepo No14 nabire papua tengah', 'NABIRE', 1, 'Router SN: HK30AYFS7D9', '43223', 'SiCepat', '45ca1ea077071403177aee044a2b614d.jpeg', '2026-03-30 09:00:27', '2026-03-30 13:35:43');
+(2, 'PT PELITA INDONESIA DJAYA', '+62 851-8307-3715', 'Jl. Raya Angkasa No.18, Kelurahan Gunung Sahari Selatan, Kemayoran, Jakarta Pusat 10610.', 'Rahmat Usman', '+62 852-4466-1641', 'Kantor pelni Nabire\r\nJalan frans kasisepo No14 nabire papua tengah', 'NABIRE', 1, 'Router SN: HK30AYFS7D9', '43223', 'Lion Parcel', '45ca1ea077071403177aee044a2b614d.jpeg', '2026-03-30 09:00:27', '2026-03-30 15:17:55'),
+(3, 'PT PELITA INDONESIA DJAYA', '+62 851-8307-3715', 'Jl. Raya Angkasa No.18, Kelurahan Gunung Sahari Selatan, Kecamatan Kemayoran, Jakarta Pusat, 10610.', 'FATUR/ABDURRACHMAN BAPA', '-', '-', 'FAK-FAK', 1, '-', '11LP1774323745821', 'Lion Parcel', 'b8493ca8407df27df4d56a98162485ab.jpeg', '2026-03-30 15:48:27', '2026-03-30 15:58:13'),
+(4, 'PT PELITA INDONESIA DJAYA', '+62 851-8307-3715', 'Jl. Raya Angkasa No.18, Kelurahan Gunung Sahari Selatan, Kecamatan Kemayoran, Jakarta Pusat, 10610.', 'IRON HUSAIN', '-', 'PT PELNI Cabang Bitung\r\nBitung Sulut, Jl. Sam Ratulangi, Bitung Barat Dua, Maesa, Bitung City, North Sulawesi', 'BITUNG', 1, '-', '11LP17744323745821', 'Lion Parcel', 'b0f4bf053c2b2a301ac10d310e6ba76a.jpeg', '2026-03-30 15:51:07', '2026-03-30 15:56:46'),
+(5, 'PT PELITA INDONESIA DJAYA', '+62 851-8307-3715', 'PT PELNI Cabang Bitung\r\nBitung Sulut, Jl. Sam Ratulangi, Bitung Barat Dua, Maesa, Bitung City, North Sulawesi', 'REZA', '-', '-', 'SAMPIT', 1, '-', '11LP1774434067944', 'Lion Parcel', '70a7bb8ea8e101c3ef3e6b3d18997fcc.jpeg', '2026-03-30 15:52:27', '2026-03-30 15:56:01');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +73,7 @@ ALTER TABLE `shipments`
 -- AUTO_INCREMENT for table `shipments`
 --
 ALTER TABLE `shipments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
