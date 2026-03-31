cec4b4406ea8086d8ca1d83308f3bb9ac0fbac75
rqh3121 <muhammadkurniasandi26@gmail.com>
2026-03-31 09:42:19 +0700

    label04

--- application/controllers/Shipment.php
+++ application/controllers/Shipment.php
@@ -8,9 +8,10 @@ class Shipment extends CI_Controller {
         require_once FCPATH . 'vendor/autoload.php';
         $this->load->model('Shipment_model');
         $this->load->library('form_validation');
-        $this->load->helper('image'); // untuk img_to_base64 jika diperlukan, tapi di view kita pakai base_url()
+        $this->load->helper('image'); // helper untuk base64 gambar (opsional)
     }
 
+    // Daftar semua pengiriman
     public function index() {
         $search = $this->input->get('search');
         $data['shipments'] = $this->Shipment_model->get_shipments($search);
@@ -20,12 +21,16 @@ class Shipment extends CI_Controller {
         $this->load->view('templates/footer');
     }
 
+    // Form tambah
     public function create() {
+        $data['city_options'] = $this->get_city_list();
+        $data['address_options'] = $this->get_address_list();
         $this->load->view('templates/header');
-        $this->load->view('shipments/create');
+        $this->load->view('shipments/create', $data);
         $this->load->view('templates/footer');
     }
 
+    // Simpan data baru
     public function store() {
         $this->form_validation->set_rules('sender_name', 'Nama Pengirim', 'required');
         $this->form_validation->set_rules('sender_contact', 'Kontak Pengirim', 'required');
@@ -40,16 +45,27 @@ class Shipment extends CI_Controller {
         if ($this->form_validation->run() == FALSE) {
             $this->create();
         } else {
+            // Proses manual input untuk kota dan alamat
+            $receiver_city = $this->input->post('receiver_city');
+            if ($receiver_city == 'other') {
+                $receiver_city = $this->input->post('receiver_city_manual');
+            }
+
+            $receiver_address = $this->input->post('receiver_address');
+            if ($receiver_address == 'other') {
+                $receiver_address = $this->input->post('receiver_address_manual');
+            }
+
             $data = [
-                'sender_name'    => $this->input->post('sender_name'),
-                'sender_contact' => $this->input->post('sender_contact'),
-                'sender_address' => $this->input->post('sender_address'),
-                'receiver_name'  => $this->input->post('receiver_name'),
+                'sender_name'     => $this->input->post('sender_name'),
+                'sender_contact'  => $this->input->post('sender_contact'),
+                'sender_address'  => $this->input->post('sender_address'),
+                'receiver_name'   => $this->input->post('receiver_name'),
                 'receiver_contact'=> $this->input->post('receiver_contact'),
-                'receiver_address'=> $this->input->post('receiver_address'),
-                'receiver_city'  => $this->input->post('receiver_city'),
-                'package_count'  => $this->input->post('package_count'),
-                'item_description' => $this->input->post('item_description')
+                'receiver_address'=> $receiver_address,
+                'receiver_city'   => $receiver_city,
+                'package_count'   => $this->input->post('package_count'),
+                'item_description'=> $this->input->post('item_description')
             ];
             $this->Shipment_model->insert_shipment($data);
             $this->session->set_flashdata('success', 'Data pengiriman berhasil ditambahkan.');
@@ -57,14 +73,18 @@ class Shipment extends CI_Controller {
         }
     }
 
+    // Form edit
     public function edit($id) {
         $data['shipment'] = $this->Shipment_model->get_shipment($id);
         if (empty($data['shipment'])) show_404();
+        $data['city_options'] = $this->get_city_list();
+        $data['address_options'] = $this->get_address_list();
         $this->load->view('templates/header');
         $this->load->view('shipments/edit', $data);
         $this->load->view('templates/footer');
     }
 
+    // Update data
     public function update($id) {
         $this->form_validation->set_rules('sender_name', 'Nama Pengirim', 'required');
         $this->form_validation->set_rules('sender_contact', 'Kontak Pengirim', 'required');
@@ -79,16 +99,27 @@ class Shipment extends CI_Controller {
         if ($this->form_validation->run() == FALSE) {
             $this->edit($id);
         } else {
+            // Proses manual input untuk kota dan alamat
+            $receiver_city = $this->input->post('receiver_city');
+            if ($receiver_city == 'other') {
+                $receiver_city = $this->input->post('receiver_city_manual');
+            }
+
+            $receiver_address = $this->input->post('receiver_address');
+            if ($receiver_address == 'other') {
+                $receiver_address = $this->input->post('receiver_address_manual');
+            }
+
             $data = [
-                'sender_name'    => $this->input->post('sender_name'),
-                'sender_contact' => $this->input->post('sender_contact'),
-                'sender_address' => $this->input->post('sender_address'),
-                'receiver_name'  => $this->input->post('receiver_name'),
+                'sender_name'     => $this->input->post('sender_name'),
+                'sender_contact'  => $this->input->post('sender_contact'),
+                'sender_address'  => $this->input->post('sender_address'),
+                'receiver_name'   => $this->input->post('receiver_name'),
                 'receiver_contact'=> $this->input->post('receiver_contact'),
-                'receiver_address'=> $this->input->post('receiver_address'),
-                'receiver_city'  => $this->input->post('receiver_city'),
-                'package_count'  => $this->input->post('package_count'),
-                'item_description' => $this->input->post('item_description')
+                'receiver_address'=> $receiver_address,
+                'receiver_city'   => $receiver_city,
+                'package_count'   => $this->input->post('package_count'),
+                'item_description'=> $this->input->post('item_description')
             ];
             $this->Shipment_model->update_shipment($id, $data);
             $this->session->set_flashdata('success', 'Data pengiriman berhasil diupdate.');
@@ -96,6 +127,7 @@ class Shipment extends CI_Controller {
         }
     }
 
+    // Hapus data
     public function delete($id) {
         $shipment = $this->Shipment_model->get_shipment($id);
         if (!empty($shipment['resi_photo'])) {
@@ -107,6 +139,7 @@ class Shipment extends CI_Controller {
         redirect('shipment');
     }
 
+    // Detail pengiriman
     public function show($id) {
         $data['shipment'] = $this->Shipment_model->get_shipment($id);
         if (empty($data['shipment'])) show_404();
@@ -115,6 +148,7 @@ class Shipment extends CI_Controller {
         $this->load->view('templates/footer');
     }
 
+    // Cetak label PDF (default A5)
     public function print_label($id, $paper = 'A5') {
         $shipment = $this->Shipment_model->get_shipment($id);
         if (empty($shipment)) show_404();
@@ -136,6 +170,7 @@ class Shipment extends CI_Controller {
         $this->pdf->generate($html, $filename, $paper, 'portrait');
     }
 
+    // AJAX update resi
     public function update_resi($id) {
         if (!$this->input->is_ajax_request()) show_404();
 
@@ -181,5 +216,101 @@ class Shipment extends CI_Controller {
         $data['resi_number'] = $shipment['resi_number'];
         $data['expedition'] = $shipment['expedition'];
         $this->load->view('shipments/resi_form', $data);
+    }
+
+    // Data pilihan kota (bisa diedit sesuai kebutuhan)
+    private function get_city_list() {
+        return [
+            "PT PELNI CAB SEMARANG",
+            "PT PELNI CAB SURABAYA",
+            "PT PELNI CAB MAKASSAR",
+            "PT PELNI CAB AMBON",
+            "PT PELNI CAB MEDAN",
+            "PT PELNI CAB BATAM",
+            "PT PELNI CAB TANJUNG PINANG",
+            "PT PELNI CAB BALI",
+            "PT PELNI CAB KUMAI",
+            "PT PELNI CAB SAMPIT",
+            "PT PELNI CAB BATULICIN",
+            "PT PELNI CAB BALIKPAPAN",
+            "PT PELNI CAB TIMIKA",
+            "PT PELNI CAB TARAKAN",
+            "PT PELNI CAB JAYAPURA",
+            "PT PELNI CAB PAREPARE",
+            "PT PELNI CAB BAUBAU",
+            "PT PELNI CAB KENDARI",
+            "PT PELNI CAB LARANTUKA",
+            "PT PELNI CAB PALU",
+            "PT PELNI CAB MAUMERE",
+            "PT PELNI CAB KUPANG",
+            "PT PELNI CAB LABUAN BAJO",
+            "PT PELNI CAB KAIMANA",
+            "PT PELNI CAB SORONG",
+            "PT PELNI CAB MANOKWARI",
+            "PT PELNI CAB LUWUK",
+            "PT PELNI CAB BITUNG",
+            "PT PELNI CAB PONTIANAK",
+            "PT PELNI CAB BIMA",
+            "PT PELNI CAB TERNATE",
+            "PT PELNI CAB MERAUKE",
+            "PT PELNI CAB WAINGAPU",
+            "PT PELNI CAB ENDE",
+            "PT PELNI CAB NABIRE",
+            "PT PELNI CAB NUNUKAN",
+            "PT PELNI CAB FAKFAK",
+            "PT PELNI CAB SERUI",
+            "PT PELNI CAB DOBO",
+            "PT PELNI CAB TUAL",
+            "PT PELNI CAB NAMLEA",
+            "PT PELNI CAB BIAK"
+
+        ];
+    }
+
+    // Data pilihan alamat (bisa diedit sesuai kebutuhan)
+    private function get_address_list() {
+        return [
+            "JL. EMPU TANTULAR NO.25, BANDARHARJO, SEMARANG UTARA, SEMARANG, JAWA TENGAH",
+            "JL. PAHLAWAN NO.112, KREMBANGAN, SURABAYA, JAWA TIMUR",
+            "JL. JENDERAL SUDIRMAN NO.14, SAWERIGADING, UJUNG PANDANG, MAKASSAR, SULAWESI SELATAN",
+            "JL. D.I. PANJAITAN NO.19, URITETU, SIRIMAU, AMBON, MALUKU",
+            "JL. GUNUNG KRAKATAU NO.17A, MEDAN, SUMATERA UTARA",
+            "JL. DR. CIPTO MANGUNKUSUMO NO.4 TANJUNG PINGGIR, SEKUPANG, BATAM",
+            "JL. JEND. AHMAD YANI NO. 06 (KM. 5 ATAS) KEL. SEI JENG KEC. BUKIT BESTARI, TANJUNG PINANG, KEPULAUAN RIAU",
+            "JL. RAYA KUTA NO. 299, TUBAN, BADUNG, BALI.",
+            "Jl. SUDIRMAN SH No. 16, KEL. SIDOREJO PANGKALAN BUN, KEC. ARUT SELATAN, KAB. KOTAWARINGIN BARAT, KALIMANTAN TENGAH",
+            "JL. A. YANI NO. 70, KEL. MENTAWA BARU HULU, KEC. MENTAWA BARU KETAPANG, KAB KOTAWARINGIN TIMUR, KALIMANTAN TENGAH, 74322 PT. PELNI CAB. BATULICIN JALAN RAYA BATULICIN, KAMPUNG BARU, KEC. SIMPANG EMPAT, KAB. TANAH BUMBU, KALIMANTAN SELATAN, 72212",
+            "JL. YOS SUDARSO NO.1 KEL. PRAPATAN, KEC. BALIKPAPAN, KOTA BALIKPAPAB, KALIMANTAN TIMUR 76111",
+            "JL. KARTINI NO. 5, KEL. INAUGA, DISTRIK MIMIKA BARU, KAB. TIMIKA, PROV. PAPUA TENGAH 99971",
+            "JL. KUSUMA BANGSA RT/RW 07/03 NO. 100, KEL. GUNUNG LINGKAS, KEC. TARAKAN TIMUR, KAB. TARAKAN, PROV. KALIMANTAN UTARA 77126",
+            "JL. ARGAPURA NO.15, ARGAPURA, DISTRIK JAYAPURA SELATAN, KOTA JAYAPURA, PAPUA",
+            "JL. LASIMING NO.44, UJUNG, PARE-PARE, SULAWESI SELATAN",
+            "JL. PAHLAWAN NO.1 BAU-BAU, BUTON, SULAWESI TENGGARA",
+            "JL. LAKIDENDE KOTA LAMA NO.10, KANDAI, KENDARI, SULAWESI TENGGARA",
+            "JL. DON LORENZO DVG, LOHAYONG, LARANTUKA, FLORES TIMUR, NUSA TENGGARA TIMUR",
+            "JL. RA KARTINI NO.96, PALU TIMUR, PALU, SULAWESI TENGAH",
+            "JL. DON JUAN NO.6, ALOK, SIKKA, FLORES, NUSA TENGGARA TIMUR",
+            "JL. PAHLAWAN NO.7, FATUFETO, ALAK, KUPANG, NUSA TENGGARA TIMUR",
+            "JL. TRANS FLORES, PASAR BARU, MANGGARAI BARAT, NUSA TENGGARA TIMUR",
+            "JL. DIPONEGORO, KAIMANA, PAPUA BARAT",
+            "JL. JEND. A. YANI KOMP. PELABUHAN SORONG - PAPUA BARAT",
+            "JL. SILIWANGI NO. 24, MANOKWARI BARAT, MANOKWARI, PAPUA BARAT",
+            "JL. SUNGAI LIMBOTO NO. 74, BUNGIN, LUWUK, BANGGAI, SULAWESI TENGAH",
+            "JL. SAM RATULANGI NO. 7, BITUNG, SULAWESI UTARA",
+            "JL. SULTAN ABDURAHMAN NO.12, SUNGAI BANGKONG, PONTIANAK, KALIMANTAN BARAT",
+            "JL. KESATRIA NO.2, PENATOI, MPUNDA, BIMA, NUSA TENGGARA BARAT",
+            "JL. JEND. A. YANI KOMP. PELABUHAN TERNATE, MALUKU UTARA",
+            "JL. SABANG NO. 318, MERAUKE, PAPUA SELATAN",
+            "JL. HASANUDIN NO.1 WAINGAPU SUMBA TIMUR, NUSA TENGGARA TIMUR",
+            "JL. KATEDRAL NO.2, MBONGAWANI, ENDE SELATAN, NUSA TENGGARA TIMUR",
+            "JL. FRANS KAISEPO NO. 14, NABIRE, PAPUA",
+            "JL. AHMAD YANI NO. 15, NUNUKAN, KALIMANTAN UTARA",
+            "JL. D.I. PANJAITAN FAK FAK, PAPUA BARAT",
+            "JL. DR. WAHIDIN SUDIROHUSODO, KEP. YAPEN, SERUI, PAPUA",
+            "JL. YOS SUDARSO NO.22, GALAI DUBU, KEP. ARU, DOBO, MALUKU",
+            "JL. AHMAD YANI NO.2, LODAR EL, TUAL, MALUKU",
+            "JL. BTN TATANGGO, NAMLEA, BURU, MALUKU",
+            "JL. JEND. SUDIRMAN NO. 37, BUROKUB, BIAK KOTA, BIAK NUMFOR, PAPUA",
+        ];
     }
 }
--- application/views/shipments/edit.php
+++ application/views/shipments/edit.php
@@ -1,10 +1,12 @@
-<div class="card shadow">
-    <div class="card-header"><h4>Edit Pengiriman</h4></div>
+<div class="card shadow-sm">
+    <div class="card-header bg-white py-3">
+        <h4 class="mb-0"><i class="fas fa-edit me-2"></i>Edit Pengiriman #<?= $shipment['id'] ?></h4>
+    </div>
     <div class="card-body">
         <?= form_open('shipment/update/'.$shipment['id']) ?>
         <div class="row">
             <div class="col-md-6">
-                <h5>Data Pengirim</h5>
+                <h5 class="border-start border-primary border-4 ps-3 mb-3">Data Pengirim</h5>
                 <div class="mb-3">
                     <label>Nama Pengirim</label>
                     <input type="text" name="sender_name" class="form-control" value="<?= set_value('sender_name', $shipment['sender_name']) ?>" required>
@@ -19,7 +21,7 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <h5>Data Penerima</h5>
+                <h5 class="border-start border-success border-4 ps-3 mb-3">Data Penerima</h5>
                 <div class="mb-3">
                     <label>Nama Penerima (PIC)</label>
                     <input type="text" name="receiver_name" class="form-control" value="<?= set_value('receiver_name', $shipment['receiver_name']) ?>" required>
@@ -29,25 +31,37 @@
                     <input type="text" name="receiver_contact" class="form-control" value="<?= set_value('receiver_contact', $shipment['receiver_contact']) ?>" required>
                 </div>
                 <div class="mb-3">
-                    <label>Alamat Penerima</label>
-                    <textarea name="receiver_address" class="form-control" rows="2" required><?= set_value('receiver_address', $shipment['receiver_address']) ?></textarea>
+                    <label>Kota Penerima</label>
+                    <input list="city-list" name="receiver_city" class="form-control" value="<?= set_value('receiver_city', $shipment['receiver_city']) ?>" required>
+                    <datalist id="city-list">
+                        <?php foreach($city_options as $city): ?>
+                            <option value="<?= htmlspecialchars($city) ?>">
+                        <?php endforeach; ?>
+                    </datalist>
                 </div>
                 <div class="mb-3">
-                    <label>Kota Penerima</label>
-                    <input type="text" name="receiver_city" class="form-control" value="<?= set_value('receiver_city', $shipment['receiver_city']) ?>" required>
+                    <label>Alamat Penerima</label>
+                    <input list="address-list" name="receiver_address" class="form-control" value="<?= set_value('receiver_address', $shipment['receiver_address']) ?>" required>
+                    <datalist id="address-list">
+                        <?php foreach($address_options as $addr): ?>
+                            <option value="<?= htmlspecialchars($addr) ?>">
+                        <?php endforeach; ?>
+                    </datalist>
                 </div>
                 <div class="mb-3">
                     <label>Jumlah Paket</label>
                     <input type="number" name="package_count" class="form-control" value="<?= set_value('package_count', $shipment['package_count']) ?>" min="1" required>
                 </div>
                 <div class="mb-3">
-                    <label>Keterangan Barang</label>
+                    <label>Keterangan Barang (opsional)</label>
                     <textarea name="item_description" class="form-control" rows="2"><?= set_value('item_description', $shipment['item_description']) ?></textarea>
                 </div>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Update</button>
-        <a href="<?= site_url('shipment') ?>" class="btn btn-secondary">Batal</a>
+        <div class="text-end mt-3">
+            <button type="submit" class="btn btn-primary"><i class="fas fa-save me-1"></i> Update</button>
+            <a href="<?= site_url('shipment') ?>" class="btn btn-secondary"><i class="fas fa-times me-1"></i> Batal</a>
+        </div>
         <?= form_close() ?>
     </div>
 </div>
